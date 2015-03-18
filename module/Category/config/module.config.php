6637e5981e17ac19bda17ee82a926404f565b6a7
zhenqing <zhaozhenqing@gmail.com>
2015-03-18 10:18:44 -0700

    config the Category index

--- module/Category/config/module.config.php
+++ module/Category/config/module.config.php
@@ -1,3 +1,29 @@
 <?php
 return array(
-);
+     'controllers' => array(
+         'invokables' => array(
+             'Category\Controller\Index' => 'Category\Controller\IndexController',
+         ),
+     ),
+      // The following section is new and should be added to your file
+     'router' => array(
+         'routes' => array(
+             'category_home' => array(
+                 'type'    => 'Zend\Mvc\Router\Http\Literal',
+                 'options' => array(
+                     'route'    => '/category',
+                     'defaults' => array(
+                         'controller' => 'Category\Controller\Index',
+                         'action'     => 'index',
+                     ),
+                 ),
+             ),
+         ),
+     ),
+
+     'view_manager' => array(
+         'template_path_stack' => array(
+             'category' => __DIR__ . '/../view',
+         ),
+     ),
+ );
---
+++ module/Category/src/Category/Controller/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace Category\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class IndexController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    	
+    	// $categoryService = $this->getServiceLocator()->get("Category\Model\CategoryTable");
+     //      //var_dump(get_class($categoryService));
+     //      $categories = $categoryService->fetchAll();
+     //      $categories->buffer();
+     //      return array('categories'=>$categories);
+    }
+   
+}
