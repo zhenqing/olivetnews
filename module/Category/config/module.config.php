b42ce56c33b03c7447698b7ae00a29ed5b559b47
zhenqing <zhaozhenqing@gmail.com>
2015-03-18 12:07:23 -0700

    add categoryController

--- module/Category/config/module.config.php
+++ module/Category/config/module.config.php
@@ -3,24 +3,34 @@ return array(
      'controllers' => array(
          'invokables' => array(
              'Category\Controller\Index' => 'Category\Controller\IndexController',
+             'Category\Controller\Category' => 'Category\Controller\CategoryController',
          ),
      ),
-      // The following section is new and should be added to your file
+    
      'router' => array(
          'routes' => array(
-             'category_home' => array(
+            'home' => array(
                  'type'    => 'Zend\Mvc\Router\Http\Literal',
                  'options' => array(
-                     'route'    => '/category',
+                     'route'    => '/',
                      'defaults' => array(
                          'controller' => 'Category\Controller\Index',
                          'action'     => 'index',
                      ),
                  ),
              ),
+             'category_home' => array(
+                 'type'    => 'Zend\Mvc\Router\Http\Literal',
+                 'options' => array(
+                     'route'    => '/category',
+                     'defaults' => array(
+                         'controller' => 'Category\Controller\Category',
+                         'action'     => 'index',
+                     ),
+                 ),
+             ),
          ),
      ),
-
      'view_manager' => array(
          'template_path_stack' => array(
              'category' => __DIR__ . '/../view',
