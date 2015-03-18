4d8711cb3fd75c6593276b1d4ad08061955179f8
zhenqing <zhaozhenqing@gmail.com>
2015-03-18 13:57:23 -0700

    show posts in each category

--- module/Category/src/Category/Controller/CategoryController.php
+++ module/Category/src/Category/Controller/CategoryController.php
@@ -10,12 +10,14 @@ class CategoryController extends AbstractActionController
 {
     public function indexAction()
     {
-    	
+    	$cat_id=(int) $this->params()->fromRoute('categoryid', 0);;
     	$categoryService = $this->getServiceLocator()->get("Category\Model\CategoryTable");
-          var_dump(get_class($categoryService));
-          $categories = $categoryService->fetchAll();
-          $categories->buffer();
-          return array('categories'=>$categories);
+        $categories = $categoryService->fetchAll();
+        $categories->buffer();
+
+    	$postService = $this->getServiceLocator()->get("Category\Model\PostTable");
+		$posts = $postService->getPostsByCategoryId($cat_id);
+		return array('categories'=>$categories,'posts'=>$posts);
     }
    public function manageAction()
     {
